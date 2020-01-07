3ebbf3f955d5832a311c0f935e4097eb3a52da29
RageLeague <34314733+RageLeague@users.noreply.github.com>
2020-01-06 21:45:26 -0700

    Move Credit Page

--- thirsk_outer_space/lib/pages/home_page.dart
+++ thirsk_outer_space/lib/pages/home_page.dart
@@ -2,115 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:thirsk_outer_space/calendar/calendar.dart';
-import 'package:thirsk_outer_space/general/common_widgets.dart';
 import 'package:thirsk_outer_space/strings/string_getter.dart';
 import 'package:thirsk_outer_space/lunch_menu/menu_display.dart';
-import 'package:thirsk_outer_space/general/version_number.dart';
 
-/// The page that displays the people who made this app
-class CreditPage extends StatelessWidget{  //Development credits page
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      color: Colors.grey[800],
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            new Container(
-              height: 30.0,
-
-            ),
-
-            PreviousPageButton(),
-            //back button to return to previous page
-
-            new Container(
-              height: 20.0,
-
-            ),
-
-            new Image(
-              image: new AssetImage('assets/icf.png'),
-              height: 160,
-            ),
-
-            new Container(
-              height: 10.0,
-
-            ),
-
-            new Text(
-              getString('credit/app_title'),
-              style: new TextStyle(
-                fontFamily: 'ROCK',
-                letterSpacing: 4,
-                fontSize: 22,
-                color: Color(0xFF5d9dfa),
-              ),
-            ),
-
-            new Text(
-              "${getString('credit/version')}: ${appInfo.version}" +
-                (appInfo.buildNumber != null && appInfo.buildNumber != "" ? "+${appInfo.version}" : ""),
-              style: new TextStyle(
-                fontFamily: 'ROCK',
-                fontSize: 12,
-                color: Color(0xFF5d9dfa),
-                letterSpacing: 2,
-              ),
-            ),
-
-            new Container(
-              height: 20.0,
-            ),
-
-            new Text(
-              getString('credit/2018/header'),
-              style: new TextStyle(
-                  fontFamily: 'ROCK',
-                  fontSize: 22,
-                  color: Colors.white,
-                  letterSpacing: 2),
-            ),
-
-            new Container(
-              height: 3.0,
-
-            ),
-
-            new Text(
-              getString('credit/2018/credit'),
-              textAlign: TextAlign.left,
-              style: TextStyle(color: Colors.white, fontSize: 14),
-
-            ),
-            new Container(height:20.0),
-            new Text(
-              getString('credit/2019/header'),
-              style: new TextStyle(
-                  fontFamily: 'ROCK',
-                  fontSize: 22,
-                  color: Colors.white,
-                  letterSpacing: 2),
-            ),
-
-            new Container(
-              height: 3.0,
-
-            ),
-
-            new Text(
-              getString('credit/2019/credit'),
-              textAlign: TextAlign.left,
-              style: TextStyle(color: Colors.white, fontSize: 14),
-
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-} //Dev Credits Page
 /// The home page of the application. Displays on startup.
 class HomePage extends StatelessWidget{
 
@@ -118,18 +12,9 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) { //builds the page
     return new Container( child: ListView ( //dictates page format
       children: <Widget>[
-
-        new RawMaterialButton(
-          child: Image.asset('assets/title.png', ),
-          onPressed: () {
-            Navigator.push(context,
-              MaterialPageRoute(builder: (context) => CreditPage()),
-            );
-          },
-        ),
+        Image.asset('assets/title.png', ),
         // thirskOS logo at top of home page
-        // the image also serves as a secret button, once tapped it takes the user to the development credits page
-
+        
         new Container(
           height: 5.0,
         ), //these containers act as spacers between pieces of content on the page
--- thirsk_outer_space/lib/pages/settings_page.dart
+++ thirsk_outer_space/lib/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thirsk_outer_space/general/common_widgets.dart';
+import 'package:thirsk_outer_space/general/general_functions.dart';
 import 'package:thirsk_outer_space/strings/string_getter.dart';
 import 'package:thirsk_outer_space/general/version_number.dart';
 
@@ -29,7 +30,7 @@ class SettingsPage extends StatelessWidget{
                 Text("Credits"),
               ],
             ),
-            onPressed: ()=>{},
+            onPressed: () => goToPage(context, CreditPage()),
           ),
           Container(height: 5.0,),
           ThriveButton(
@@ -57,3 +58,108 @@ class SettingsPage extends StatelessWidget{
     );
   }
 } //Home Page
+
+/// The page that displays the people who made this app
+class CreditPage extends StatelessWidget{  //Development credits page
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.grey[800],
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            new Container(
+              height: 30.0,
+
+            ),
+
+            PreviousPageButton(),
+            //back button to return to previous page
+
+            new Container(
+              height: 20.0,
+
+            ),
+
+            new Image(
+              image: new AssetImage('assets/icf.png'),
+              height: 160,
+            ),
+
+            new Container(
+              height: 10.0,
+
+            ),
+
+            new Text(
+              getString('credit/app_title'),
+              style: new TextStyle(
+                fontFamily: 'ROCK',
+                letterSpacing: 4,
+                fontSize: 22,
+                color: Color(0xFF5d9dfa),
+              ),
+            ),
+
+            new Text(
+              "${getString('credit/version')}: ${appInfo.version}" +
+                (appInfo.buildNumber != null && appInfo.buildNumber != "" ? "+${appInfo.version}" : ""),
+              style: new TextStyle(
+                fontFamily: 'ROCK',
+                fontSize: 12,
+                color: Color(0xFF5d9dfa),
+                letterSpacing: 2,
+              ),
+            ),
+
+            new Container(
+              height: 20.0,
+            ),
+
+            new Text(
+              getString('credit/2018/header'),
+              style: new TextStyle(
+                  fontFamily: 'ROCK',
+                  fontSize: 22,
+                  color: Colors.white,
+                  letterSpacing: 2),
+            ),
+
+            new Container(
+              height: 3.0,
+
+            ),
+
+            new Text(
+              getString('credit/2018/credit'),
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.white, fontSize: 14),
+
+            ),
+            new Container(height:20.0),
+            new Text(
+              getString('credit/2019/header'),
+              style: new TextStyle(
+                  fontFamily: 'ROCK',
+                  fontSize: 22,
+                  color: Colors.white,
+                  letterSpacing: 2),
+            ),
+
+            new Container(
+              height: 3.0,
+
+            ),
+
+            new Text(
+              getString('credit/2019/credit'),
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.white, fontSize: 14),
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+} //Dev Credits Page
